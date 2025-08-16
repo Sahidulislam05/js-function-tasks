4e4dd43e9fdd69576d81f706213c288886bca130
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-08-16 13:52:31 +0600

    added function

---
+++ test.java
@@ -0,0 +1,21 @@
+/** 
+ * package method;
+public class method {
+    public static void main(String []args){
+        A obj= new A();
+        obj.add(5.5);
+        
+    }
+}
+public class A{
+    public void add() {
+        System.out.println("No value");
+    }
+    public void add(int a, int b) {
+        System.out.println(a+b);
+    }
+    public void add(double a) {
+        System.out.println(a *a );
+    }
+}
+ */
